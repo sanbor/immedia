6afc0882272cd168808f7b56224f3d16ee351f83
Julian Cerruti <jcerruti@willowgarage.com>
2013-07-05 23:42:35 -0300

    Complete rewrite, first pass done

--- app.js
+++ app.js
@@ -32,77 +32,83 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/maker', routes.maker);
 
-var maker;
-var makerIceCandidates = [];
-var makerSdp;
-var audience = [];  // Array of { name: , socket: , sdp: , candidates: }
+/**
+ * State is handled as follows:
+ * - In the 'globalState' variable below, comprised of the next available
+ *   ID to give a peer and a dictionary of peer 'state' objects, keyed
+ *   by peer ID.
+ * - Each 'state' object has:
+ *   - type: ['master' or 'slave' or whatever]
+ *   - id: the peer ID
+ *   - socket: link to the socket.io socket object
+ * - The 'state' object is also set to the socket as its 'state' propery
+ */
+var globalState = {
+  nextId: 1,
+  peers: {}
+};
+
+// TODO: Find what is the best way to do this on Node JS
+function keys(anObject) {
+  var keys = [];
+  for(var key in anObject) {
+    keys.push(key);
+  }
+  return keys;
+}
+
+function createPeer(socket, role) {
+  // A new peer is registering
+  // 1- Allocate an ID and send back to client
+  var newPeerId = role + ":" + globalState.nextId++;
+  state = {
+    type: role,
+    id: newPeerId,
+    socket: socket
+  };
+  console.log('Creating new peer: ' + newPeerId);
+  // TODO: Maybe replace by returning data via ACK
+  socket.emit('id', newPeerId);
+
+  // 2- Send current list of peers
+  // TODO: Use broadcast instead
+  socket.emit('peer', {'add': keys(globalState.peers) });
+
+  // 3- Notify other peers of this new client
+  for(var peerId in globalState.peers) {
+    globalState.peers[peerId].socket.emit('peer', {'add': [newPeerId] });
+  }
+
+  // 4- Save this new peer in our globalState and in the socket
+  globalState.peers[newPeerId] = state;
+  socket.state = state;
+}
 
 io.sockets.on('connection', function(socket) {
-  socket.on('maker',function() {
-    if(maker === undefined) {
-      maker = socket;
-    }
-    if(audience.length > 0) {
-      socket.emit('audience all', audience.map(function(entry) { return entry.name; }));
-    }
+  socket.on('master',function() {
+    createPeer(socket, 'master');
   });
-  socket.on('audience', function(name) {
-    console.log('New watcher: ' + name);
-    audience.push({ name: name, socket: socket });
-    if( maker) {
-      maker.emit('audience add', name);
-    }
-    if(makerSdp) {
-      socket.emit('webrtc sdp', makerSdp);
-    }
-    if(makerIceCandidates.length > 0) {
-      socket.emit('webrtc candidates', makerIceCandidates);
-    }
+  socket.on('slave', function(name) {
+    createPeer(socket, 'slave');
   });
   socket.on('disconnect', function() {
-    if(socket == maker) {
-      maker = undefined;
-      makerIceCandidates = [];
-      makerSdp = undefined;
-      audience.forEach(function(entry){ entry.socket.emit('maker left'); });
-    } else {
-      var left = audience.filter(function(entry, ix) { if(entry.socket == socket) { return audience.splice(ix,1); } });
-      if(left.length > 0 && maker !== undefined) {
-        console.log(left[0].name + ' left');
-        maker.emit('audience del', left[0].name);
-      }
+    if(!socket.state)
+      // It never even registered as slave or master
+      // e.g. fast reload
+      return;
+    delete globalState.peers[socket.state.id];
+    for(var peerId in globalState.peers) {
+      globalState.peers[peerId].socket.emit('peer', {'del': [socket.state.id] });
     }
   });
 
   // WebRTC
-  socket.on('webrtc candidate', function(candidate) {
-    if(socket == maker) {
-      makerIceCandidates.push(candidate);
-      audience.forEach(function(entry){ entry.socket.emit('webrtc candidate', candidate); });
-    } else {
-      var audienceEntry = audience.filter(function(entry) { return (entry.socket == socket); });
-      if( audienceEntry.length > 0) {
-        if( ! audienceEntry.candidates) { audienceEntry.candidates = []; }
-        audienceEntry.candidates.push(candidate);
-        if(maker) {
-          maker.emit('webrtc candidate', candidate);
-        }
-      }
-   }
-  });
-  socket.on('webrtc sdp', function(sdp) {
-    if(socket == maker) {
-      makerSdp = sdp
-      audience.forEach(function(entry){ entry.socket.emit('webrtc sdp', sdp); });
-    } else {
-      var audienceEntry = audience.filter(function(entry) { return (entry.socket == socket); });
-      if( audienceEntry.length > 0) {
-        audienceEntry.sdp = sdp;
-        if(maker) {
-          maker.emit('webrtc sdp', sdp);
-        }
-      }
-    }
+  socket.on('webrtc', function(payload) {
+    var peerSocket = globalState.peers[payload.peerId];
+    // Change the ID in peerId to be the sender instead of
+    // the recipient, which is what comes in on the message
+    payload.peerId = socket.state.id;
+    peerSocket.socket.emit('webrtc', payload);
   });
 });
 
--- public/js/craud.common.js
+++ public/js/craud.common.js
@@ -1,6 +1,174 @@
-CROUD = {};
-
 function timeLog() {
   [].unshift.apply(arguments, ["[" + Math.round((new Date().getTime())/10)%10000 + "]"]);
   console.log.apply(console, arguments);
 }
+
+function keys(anObject) {
+  var keys = [];
+  for(var key in anObject) {
+    keys.push(key);
+  }
+  return keys;
+}
+
+
+$(function() {
+  timeLog("Starting Maker component");
+
+  var state = {
+    id: undefined,
+    channel: undefined,
+    stream: undefined,
+    peers: {},
+    peerConnections: {}
+  };
+
+  // Get access to Webcam
+  getUserMedia({audio: true, video: true}, function(stream) {
+    state.stream = stream;
+    goAttachLocalStream(stream);
+    goReady();
+  });
+
+  // Open socket.io channel
+  var channel = io.connect();
+  channel.on('connect', function() {
+    state.channel = channel;
+    goReady();
+  });
+  channel.on('id', function(myId) {
+    state.id = myId;
+    goOnline();
+  });
+
+  // Peer connect/disconnect events
+  channel.on('peer', function(data) {
+    // TODO: Decide to pay attention only to a particular type of peer
+    // a.k.a. Filter peers. Use prefix in the ID
+    if('add' in data) {
+      data['add'].forEach(function(peerId) {
+        state.peers[peerId] = {};
+        goPeerAdded(peerId);
+      });
+    }
+    if('del' in data) {
+      data['del'].forEach(function(peerId) {
+        goPeerRemoved(peerId);
+        delete state.peers[peerId];
+      });
+    }
+  });
+
+  // Self disconnect event
+  channel.on('disconnect', function() {
+    // Tear down all connections
+    keys(state.peerConnection).forEach(function(peerId) {
+      goDisconnect(peerId);
+    });
+    // Forget any information we think we have about connected peers
+    state.peers = {};
+    // And about ourselves
+    state.channel = undefined;
+    state.id = undefined;
+  });
+
+  function goReady() {
+    // Don't start unless both socket.io and getUserMedia succeeded
+    if(state.channel === undefined || state.stream === undefined) 
+      return;
+
+    channel.emit(config.self);
+  }
+
+  function goOnline() {
+    // If we already know of any waiting peers, trigger a start with them
+    keys(state.peers).forEach(function(peerId) {
+      goPeerAdded(peerId);
+    });
+  }
+
+  function goPeerAdded(peerId) {
+    if(config.self == 'master'/* TODO: Configure auto-connect per-client */) {
+      goConnect(peerId);
+    }
+  }
+  function goPeerRemoved(peerId) {
+    if(peerId in state.peerConnections) {
+      goDisconnect(peerId);
+    }
+  }
+
+  function goConnect(peerId) {
+    $('#status').html('On Call with ' + peerId);
+    var pcState = state.peerConnections[peerId] = {};
+    pcState.role = 'slave';
+    var pc = pcState.pc = makeWebRTCPC(peerId, state.stream);
+    pc.createOffer(function(sdp) {
+      pc.setLocalDescription(sdp);
+      channel.emit('webrtc', {
+        peerId: peerId,
+        sdp: sdp
+      });
+    });
+  }
+  function goDisconnect(peerId) {
+    $('#status').html('Disconnected from ' + peerId);
+    // Cancel potential callbacks about this connection
+    var pc = state.peerConnections[peerId].pc;
+    pc.onicecandidate = pc.onaddstream = function() {};
+    // TODO: Do a proper and clean disconnection from PC
+    // TODO: Detach video from stream, if it had been attached
+    delete state.peerConnections[peerId];
+  }
+
+  channel.on('webrtc', function(message) {
+    var peerId = message.peerId;
+    if(!(peerId in state.peerConnections)) {
+      // We're getting messages from a peer we haven't initiated
+      // connection to. We must be getting 'called'
+      var pcState = state.peerConnections[peerId] = {};
+      pcState.role = 'master';
+      pcState.pc = makeWebRTCPC(peerId, state.stream);
+    }
+    var pcState = state.peerConnections[peerId];
+    if('candidate' in message) {
+      pcState.pc.addIceCandidate(new RTCIceCandidate(message.candidate));
+    }
+    if('sdp' in message) {
+      pcState.pc.setRemoteDescription(new RTCSessionDescription(message.sdp));
+      if(pcState.role = 'master') {
+        pcState.pc.createAnswer(function(sdp) {
+          pcState.pc.setLocalDescription(sdp);
+          channel.emit('webrtc', {
+            peerId: peerId,
+            sdp: sdp
+          });
+        });
+      }
+    }
+  });
+
+  function makeWebRTCPC(peerId, stream) {
+    var pc =  new RTCPeerConnection(config.pcConfig);
+    pc.addStream(state.stream);
+    pc.onicecandidate = function(event) {
+      if(event && event.candidate) {
+        channel.emit('webrtc', {
+          peerId: peerId,
+          candidate: event.candidate
+        });
+      }
+    };
+    pc.onaddstream = function(event) {
+      goAttachPeerStream(peerId, event.stream);
+    };
+    return pc;
+  }
+
+  function goAttachLocalStream(stream) {
+    attachMediaStream($('#local-video')[0], stream);
+  }
+  function goAttachPeerStream(peerId, stream) {
+    attachMediaStream($('#remote-video')[0], stream);
+  }
+});
